7887dc961809376bc50791e95ef3502e4e20ae08
Mukut Chakrabarty <mukutckb1@gmail.com>
2023-02-19 11:28:11 +0600

    Header-created

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes  } from 'react-router-dom';
+import CreatePost from './pages/CreatePost';
+import Home from './pages/Home';
+import logoImg from './assets/logo.svg'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <BrowserRouter>
+     <header className="w-full flex justify-between items-center bg-white sm:px-8 py-4 border-b border-b-[#e6ebf4]">
+            <Link to="/">
+                <img src={logoImg} alt='image-generator' className='w-28 object-contain' />
+            </Link>
+            <Link to="/create-post" className='font-inter font-medium bg-indigo-600 text-white px-4 py-2 rounded-md'>Create</Link>
+    </header>
+    
+    <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+         <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/create-post' element={<CreatePost/>}></Route>
+         </Routes>
+    </main>
 
-export default App
+    </BrowserRouter>
+  );
+};
+
+export default App;
